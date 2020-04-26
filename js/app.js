9bdf91d20d5ed5dabbf69d6c5e8d3dcc8c764ada
渡部森太郎 <shintaro@watanabemoritarou-no-MacBook-Air.local>
2020-04-26 18:57:52 +0900

    uu

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const clickBtn = ()=>{
     let textarea = document.getElementById('textarea').value;
-    result = textarea.replace(/\n/g, '\r\n');
+    result = textarea.replace(/\n/g, '');
     console.log(result);
     document.getElementById('textarea').value = result;
     document.getElementById('textarea').select();
