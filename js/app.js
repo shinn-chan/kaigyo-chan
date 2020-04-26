0d4a471d874f5e4baa878e655b83cefe4f183f8e
渡部森太郎 <shintaro@watanabemoritarou-no-MacBook-Air.local>
2020-04-26 19:35:55 +0900

    kaigyo

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const clickBtn = ()=>{
     let textarea = document.getElementById('textarea').value;
+    // 何に変換したらいいかわからない↓
     result = textarea.replace(/\n/g, '');
     console.log(result);
     document.getElementById('textarea').value = result;
