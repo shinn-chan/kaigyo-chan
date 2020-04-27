fdc46c5e55b0fc6e320ba5d25ce702a86f89fb06
渡部森太郎 <shintaro@watanabemoritarou-no-MacBook-Air.local>
2020-04-27 23:02:40 +0900

    add deleteBtn

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
-const clickBtn = ()=>{
-    let textarea = document.getElementById('textarea').value;
-    // 何に変換したらいいかわからない↓
-    result = textarea.replace(/\n/g, 'ㅤ\n');
-    console.log(result);
-    document.getElementById('textarea').value = result;
+const count = () =>{
+    let stringlength = document.getElementById('textarea').value.length;
+    document.getElementsByClassName('count').innerHTML.put(`文字数:${stringlength}`);
+}
+
+const submitBtn = ()=>{
+    // 変換機能
+    let textarea = '.'+'\n'+document.getElementById('textarea').value; /*文頭に改行を入れてtextareaの中身を取得*/
+    result = textarea.replace(/\n/g, 'ㅤ\n'); /*改行コードに空白文字を追加*/
+    document.getElementById('textarea').value = result;/*textareaに変換後のデーターを挿入*/
+
+    // コピー機能
     document.getElementById('textarea').select();
     document.execCommand("copy");
-    alert('コピーできやした！');
+    alert('コピー完了！インスタのテキスト欄にそのまま貼り付けてね！');
+}
+
+const deleteBtn = ()=>{
+    document.getElementById('textarea').value = "";
 }
